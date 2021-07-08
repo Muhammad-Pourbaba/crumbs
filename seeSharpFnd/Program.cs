4e5e44a500b3ae72cfdcdced33fc30c50698ec40
unkown <mr.pourbaba@gmail.com>
2021-07-08 07:28:46 -0500

    init

---
+++ seeSharpFnd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace seeSharpFnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\0fazaye khali");
+            
+        }
+    }
+}
